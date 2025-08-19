1c1622e417b67b61b86017502e67badae303ce63
AnushaBhaskar02 <anushadbhaskar2000@gmail.com>
2025-08-19 17:13:57 +0530

    done

---
+++ src/test/java/pack4/TC011_JavaScriptExecutor.java
@@ -0,0 +1,20 @@
+package pack4;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC011_JavaScriptExecutor {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://amazon.in/");
+		JavascriptExecutor js= (JavascriptExecutor)driver;
+		js.executeScript("window.scrollBy(0,1500)");
+		
+		//js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+		//js.executeScript("alert('Hello');");
+		}
+}
